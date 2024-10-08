f60eff46d931946ad37918acd916de3adeefd712
blessydr <blassy123dharmaraj@gmail.com>
2024-10-08 15:52:56 +0530

    first commit

---
+++ new.js
@@ -0,0 +1,72 @@
+console.log("hello world");
+
+alert("Wlcome to my website")
+var a=4;
+var b=6;
+console.log(a*b)
+console.table({fname:'blessy',age:19});
+console.error("hellpo")
+console.warn("danger")
+
+for(i=0;i<10;i++)
+    {
+        console.log(i)
+    }
+    var a=4;
+var b=6;
+document.getElementById("result") ;
+
+console.log(typeof("Kim Teahyung"));
+console.log(typeof(34567));
+
+{
+    let a=10;
+    console.log(a)
+}
+var a=10;
+a=22;
+
+    console.log(a);
+
+var a=10;
+console.log(a)
+var b=11;
+console.log(b)
+console.log((a==b))
+
+
+
+var z;
+console.log(z)
+
+var num=9;
+console.log(typeof(num))
+   
+console.log(["jin","j hope","teahyung","Rm"])
+// operators
+var a=12;
+a++;
+console.log(a)
+var a=12;
+
+console.log(a**2)
+
+var t=12+2-4;
+console.log(t)
+
+
+var x=6;
+console.log(x)
+
+
+var text="blessy";
+text="dr";
+console.log(text)
+
+
+
+let Rainy=true;
+let summer=true;
+
+let weather=!summer;
+console.log(weather)
